8ae15bba2438e5ae16c6784faa974de0d0990ec2
smailsai98 <124931864+smailsai98@users.noreply.github.com>
2025-11-23 10:45:17 +0100

    hello

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,6 @@
 const nextConfig = {
   // your config here
 };
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Cache-Control", value: "no-store" }
-        ],
-      },
-    ];
-  },
-};
+
 
 export default nextConfig;
